16a78b84942dc0dd2f67450dd3e7b366074d5555
Juan Gonzalez Covarrubias <104181238+jgonzalezcov@users.noreply.github.com>
2022-12-24 17:59:30 -0300

    feat:Data validation functions are built

--- app.js
+++ app.js
@@ -29,3 +29,82 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log('El servidor esta activo en el puerto', port)
 })
+
+//Funcion valida campos y notas//
+const validateInput = async (song) => {
+  let result = ''
+  if (
+    song.id === '' ||
+    song.titulo === '' ||
+    song.artista === '' ||
+    song.tono === ''
+  ) {
+    result = {
+      status: 400,
+      statusText: 'error',
+      text: 'Info Server: No se han recibido todos los parametros',
+    }
+  } else if (
+    song.tono !== 'Do' &&
+    song.tono !== 'Re' &&
+    song.tono !== 'Mi' &&
+    song.tono !== 'Fa' &&
+    song.tono !== 'Sol' &&
+    song.tono !== 'La' &&
+    song.tono !== 'Si'
+  ) {
+    result = {
+      status: 400,
+      statusText: 'error',
+      text: 'Info Server: Tono ingresado no es valido',
+    }
+  } else
+    result = {
+      status: 200,
+      statusText: 'ok',
+      text: 'La información se ha procesado correctamente',
+    }
+  return result
+}
+//Funcion valida que canción no exista al agregar//
+const validateSong = async (song) => {
+  result = ''
+  const loadInfo = JSON.parse(
+    await fsPromises.readFile(__dirname + '/data/repertorio.json', 'utf8')
+  )
+  let listSongs = loadInfo.filter(
+    (s) =>
+      s.tono === song.tono &&
+      s.titulo === song.titulo &&
+      s.artista === song.artista
+  )
+  numSongs = listSongs.length
+  if (numSongs !== 0) {
+    result = {
+      status: 400,
+      statusText: 'error',
+      text: 'Info Server: La canción ingresada ya existe',
+    }
+  }
+  return result
+}
+
+//Funcion valida que el id a eliminar o modificar exista//
+
+const validateId = async (song) => {
+  result = ''
+  const loadInfo = JSON.parse(
+    await fsPromises.readFile(__dirname + '/data/repertorio.json', 'utf8')
+  )
+
+  let idModifi = loadInfo.filter((s) => s.id === song.id).length
+  if (idModifi === 1) {
+    result = {
+      status: 400,
+      statusText: 'error',
+      text: 'tttttInfo Server: El id de la canción no existe',
+    }
+  }
+
+  return result
+}
